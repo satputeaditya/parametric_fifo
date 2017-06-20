2aa8e41fbea8e39fa91441dedaf4ff35fa32b2cc
Aditya Satpute <satputeaditya@users.noreply.github.com>
2017-06-20 16:12:25 -0700

    Update fifo_param.v

--- fifo_param.v
+++ fifo_param.v
@@ -1,4 +1,11 @@
+//======================================================================
+//
 // fifo_param.v
+// -----------
+//
+// Parametric fifo with instantiated memory
+//
+//======================================================================
 
 module fifo_param #(  parameter WIDTH = 64,      // Number  of bits
                       parameter DEPTH = 4        // Number  of bits
@@ -8,8 +15,8 @@ module fifo_param #(  parameter WIDTH = 64,      // Number  of bits
                     input  clk,
                     input  rst,
 
-                    input  push,        // push = write     
-                    input  pop,         // pop  = read    
+                    input  push,        		// push = write     
+                    input  pop,         		// pop  = read    
 
                     input  [WIDTH-1:0] data_in, 
                     output [WIDTH-1:0] data_out,
@@ -18,55 +25,67 @@ module fifo_param #(  parameter WIDTH = 64,      // Number  of bits
                     output  fifo_full
                  );
 
+  //----------------------------------------------------------------
+  // Registers declarations.
+  //----------------------------------------------------------------
+  reg [DEPTH-1:0] write_address;
+  reg [DEPTH-1:0] read_address;
+  reg [DEPTH-1:0] fifo_count;
 
+  //----------------------------------------------------------------
+  // Output port signal assignments.
+  //----------------------------------------------------------------
+  assign high_val 	= 1'b1;
+  assign low_val 	= 1'b0;
+  assign fifo_full  = (fifo_count == {DEPTH{high_val}} )?1'b1:1'b0;   // inner curly braces act as replication operator
+  assign fifo_empty = (fifo_count == {DEPTH{low_val}}  )?1'b1:1'b0;   // inner curly braces act as replication operator 
 
-reg [DEPTH-1:0] write_address;
-reg [DEPTH-1:0] read_address;
-reg [DEPTH-1:0] fifo_count;
+  //----------------------------------------------------------------
+  // generate internal write address pointer.
+  //----------------------------------------------------------------
+  always@(posedge clk or posedge rst)
+	begin
+		if (rst)
+			write_address <= 'b0;
+		else 
+		if (push == 1'b1 && (!fifo_full))                  	// if write = 1 and if fifo is NOT full then perform write operation
+			write_address <= write_address + 1'b1;
+    end
 
+  //----------------------------------------------------------------
+  // generate internal read address pointer.
+  //----------------------------------------------------------------
+  always@(posedge clk or posedge rst)
+	begin
+		if (rst)
+			read_address <= 'b0; // 256 locations
+		else
+			if (pop == 1'b1 && (!fifo_empty))              // if read = 1 and if fifo is NOT empty then perform read operation 
+				read_address <= read_address +1'b1;
+	end
 
-assign high_val = 1'b1;
-assign low_val = 1'b0;
-
-
-
-// generate internal write address
-always@(posedge clk or posedge rst)
-
-if (rst)
-    write_address <= #1 'b0;  // 256 locations
-else 
-    if (push == 1'b1 && (!fifo_full))                   // if write = 1 and if fifo is NOT full then perform write operation
-        write_address <= #1 write_address + 1'b1;
-        
-
-// generate internal read address pointer
-always@(posedge clk or posedge rst)
-if (rst)
-    read_address <= #1 'b0; // 256 locations
-else
-    if (pop == 1'b1 && (!fifo_empty))                   // if read = 1 and if fifo is NOT empty then perform read operation 
-        read_address <= #1 read_address +1'b1;
-
-
-// generate fifo count (increment on push, decrement on pop)
-always@(posedge clk or posedge rst)
-if (rst)
-    fifo_count <= #1 'b0;   // clear counter
-else
-    if (push== 1'b1 && pop == 1'b0 && (!fifo_full))
-        fifo_count <= #1 (fifo_count + 1);              // increment counter if write & fifo NOT full
-else
-    if (push== 1'b0 && pop == 1'b1 && (!fifo_empty))    // decrement counter if read & fifo NOT empty
-        fifo_count <= #1 (fifo_count - 1);
-
-// generate fifo signals
- assign fifo_full  = (fifo_count == {DEPTH{high_val}} )?1'b1:1'b0;   // A mix of parameter with curly braces will help in resolving (the inner curly brace will act as replication operator)
- assign fifo_empty = (fifo_count == {DEPTH{low_val}}  )?1'b1:1'b0;   // A mix of parameter with curly braces will help in resolving (the inner curly brace will act as replication operator)
-
+  //----------------------------------------------------------------
+  //generate fifo count (increment on push, decrement on pop)
+  //----------------------------------------------------------------
+  always@(posedge clk or posedge rst)
+	begin
+		if (rst)
+			fifo_count <= 'b0;   // clear counter
+		else
+			if (push== 1'b1 && pop == 1'b0 && (!fifo_full))
+				fifo_count <= (fifo_count + 1);              		// increment counter if write & fifo NOT full
+			else
+				if (push== 1'b0 && pop == 1'b1 && (!fifo_empty))    // decrement counter if read & fifo NOT empty
+					fifo_count <= (fifo_count - 1);
+	end
  
-// connect RAM 
-mem_param #( WIDTH , DEPTH ) memory2 (.clk(clk), .write(push), .waddr(write_address), .raddr(read_address), .wdata(data_in), .rdata(data_out) );
+  //----------------------------------------------------------------
+  // memory instantiations   
+  //----------------------------------------------------------------
+  mem_param #( WIDTH , DEPTH ) U1 (.clk(clk), .write(push), .waddr(write_address), .raddr(read_address), .wdata(data_in), .rdata(data_out) );
 
+  endmodule  // fifo_param
 
-endmodule
+//======================================================================
+// EOF fifo_param.v
+//======================================================================  
