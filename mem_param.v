811c2d7aa7e8561b662107fb2622e9fff341abea
Aditya Satpute <satputeaditya@users.noreply.github.com>
2017-06-20 16:13:33 -0700

    Update mem_param.v

--- mem_param.v
+++ mem_param.v
@@ -1,34 +1,57 @@
+//======================================================================
+//
 // mem_param.v
+// -----------
+//
+// Memory instance. 
+//
+//======================================================================
 
-module mem_param #(
+  module mem_param #(
                      parameter WIDTH = 1024,
                      parameter DEPTH = 8
                   )
-                 (
-                    input  clk,
-                    input  write,
+				  
+                  (
+                    input  				clk,
+                    input  				write,
 
-                    input  [DEPTH-1:0] waddr, 
-                    input  [DEPTH-1:0] raddr, 
+                    input  [DEPTH-1:0] 	waddr, 
+                    input  [DEPTH-1:0] 	raddr, 
 
-                    input  [WIDTH-1:0] wdata, 
-                    output [WIDTH-1:0] rdata
-                 );
+                    input  [WIDTH-1:0] 	wdata, 
+                    output [WIDTH-1:0] 	rdata
+                 );  
 
+  //----------------------------------------------------------------
+  // Registers declarations.
+  //----------------------------------------------------------------
+  reg [WIDTH-1:0] mem[0:(2**DEPTH)-1];
+  reg [WIDTH-1:0] rdatax;
 
-reg [WIDTH-1:0] mem[0:(2**DEPTH)-1];
-reg [WIDTH-1:0] rdatax;
+  //----------------------------------------------------------------
+  // Output port signal assignments.
+  //----------------------------------------------------------------
+  assign rdata = rdatax;
 
-assign rdata = rdatax;
+  //----------------------------------------------------------------
+  // memory read process 
+  //----------------------------------------------------------------
+  always @(*) 
+	begin : read_process                
+		rdatax <=  mem[raddr];
+	end  // read_process
+  //----------------------------------------------------------------
+  // memory write process 
+  //----------------------------------------------------------------
+  always @(posedge(clk)) 
+    begin : write_process
+		if(write) 
+			mem[waddr]<= wdata;
+	end  // write_process
 
-always @(*) begin               // READ 
-  rdatax <=  mem[raddr];
-end
+  endmodule // mem_param
 
-always @(posedge(clk)) begin    // WRITE
-  if(write) begin
-    mem[waddr]<= wdata;
-  end
-end
-
-endmodule
+//======================================================================
+// EOF mem_param.v
+//======================================================================
